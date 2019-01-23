d4c91a77b7c27aad6175cb6dc47d791d61c71b85
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-23 16:04:32 +0000

    Delegating tests to web_helper

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,30 +9,15 @@ feature 'Testing infrastructure' do
   feature 'Testing form for a name input' do
     scenario 'Enter into player one' do
       sign_in_and_play
-      # visit('/')
-      # player1 = 'player1'
-      # player2 = 'player2'
-      # fill_in 'player_one', with: player1
-      # fill_in 'player_two', with: player2
-      # click_button 'Submit'
-      expect(page).to have_content (player1 + " vs. " + player2)
+      expect(page).to have_content (@player1 + " vs. " + @player2)
     end
   end
 
   feature 'Testing player HP displayed' do
     scenario 'Players should have equal HP' do
       sign_in_and_play
-      # visit('/')
-      # player1 = 'player1'
-      # player2 = 'player2'
-      # fill_in 'player_one', with: player1
-      # fill_in 'player_two', with: player2
-      # click_button 'Submit'
       expect(page).to have_content(@player_1_health)
     end
 
   end
-
-
-
 end
