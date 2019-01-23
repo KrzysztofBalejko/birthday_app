200c59d491d8d2371468db3eb1e359da99934eec
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-23 15:57:32 +0000

    Delegation - creating web_helper

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,24 +8,26 @@ feature 'Testing infrastructure' do
 
   feature 'Testing form for a name input' do
     scenario 'Enter into player one' do
-      visit('/')
-      player1 = 'player1'
-      player2 = 'player2'
-      fill_in 'player_one', with: player1
-      fill_in 'player_two', with: player2
-      click_button 'Submit'
+      sign_in_and_play
+      # visit('/')
+      # player1 = 'player1'
+      # player2 = 'player2'
+      # fill_in 'player_one', with: player1
+      # fill_in 'player_two', with: player2
+      # click_button 'Submit'
       expect(page).to have_content (player1 + " vs. " + player2)
     end
   end
 
   feature 'Testing player HP displayed' do
     scenario 'Players should have equal HP' do
-      visit('/')
-      player1 = 'player1'
-      player2 = 'player2'
-      fill_in 'player_one', with: player1
-      fill_in 'player_two', with: player2
-      click_button 'Submit'
+      sign_in_and_play
+      # visit('/')
+      # player1 = 'player1'
+      # player2 = 'player2'
+      # fill_in 'player_one', with: player1
+      # fill_in 'player_two', with: player2
+      # click_button 'Submit'
       expect(page).to have_content(@player_1_health)
     end
 
