5751b687db6692a99c3152b528ae2e46c2f56412
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-25 14:30:57 +0000

    README initialized

--- spec/features/web_helpers.rb
+++
@@ -1,8 +0,0 @@
-def sign_in_and_play
-  visit('/')
-  @player1 = 'player1'
-  @player2 = 'player2'
-  fill_in 'player_one', with: @player1
-  fill_in 'player_two', with: @player2
-  click_button 'Submit'
-end
